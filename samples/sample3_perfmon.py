3abb50a28ae4534ce1a49d824dd3548c21452f5c
Aron Donaldson <ardonald@cisco.com>
2023-08-18 14:10:21 -0500

    Updating for Splunk output formatting.

--- samples/sample3_perfmon.py
+++ samples/sample3_perfmon.py
@@ -1,5 +1,6 @@
 from axltoolkit import UcmPerfMonToolkit
 from credentials import user, password, platform_user, platform_password
+import json
 
 ucm_ip = '172.18.106.58'
 
@@ -8,12 +9,11 @@ axl = UcmPerfMonToolkit(user, password, ucm_ip, False)
 session_handle = axl.perfmonOpenSession()
 
 counters = [
-    "\\\\vnt-cm1b.cisco.com\\Cisco Locations LBM(BranchRemote->Hub_None)\\BandwidthAvailable",
-    "\\\\vnt-cm1b.cisco.com\\Cisco Locations LBM(BranchRemote->Hub_None)\\BandwidthMaximum",
-    "\\\\vnt-cm1b.cisco.com\\Cisco Locations LBM(BranchRemote->Hub_None)\\CallsInProgress",
-    "\\\\vnt-cm1b.cisco.com\\Cisco Locations LBM(Hub_None)\\CallsInProgress",
-    "\\\\vnt-cm1b.cisco.com\\Cisco SIP(ecats-rtp-dmz-cube2)\\CallsAttempted",
-    "\\\\vnt-cm1b.cisco.com\\Cisco CallManager\\CallsCompleted"
+    "\\\\CHANGEME\\Cisco CallManager\\BandwidthAvailable",
+    "\\\\CHANGEME\\Cisco CallManager\\BandwidthMaximum",
+    "\\\\CHANGEME\\Cisco CallManager\\CallsInProgress",
+    "\\\\CHANGEME\\Cisco CallManager\\CallsAttempted",
+    "\\\\CHANGEME\\Cisco CallManager\\CallsCompleted"
 ]
 
 result = axl.perfmonAddCounter(session_handle=session_handle, counters=counters)
@@ -23,6 +23,14 @@ if result is True:
 else:
     result = "Error adding perfmon counter"
 
-print(result)
+
+new_result = []
+for counter in result:
+    category = counter.Name._value_1.split('\\')[-1]
+    ipAddress = counter.Name._value_1.split('\\')[2]
+    new_dict = {'category': category, 'ipAddress': ipAddress, 'value': counter.Value, 'cStatus': counter.CStatus}
+    new_result.append(new_dict)
+
+print(json.dumps(new_result))
 
 result = axl.perfmonCloseSession(session_handle=session_handle)
